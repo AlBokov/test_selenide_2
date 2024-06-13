1a44f3dcaad05f5475d7471666c726d954a4ebb9
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-06-13 18:23:58 +0300

    update test

--- src/test/java/ru.netology.web/OrderingCardDelivery.java
+++ src/test/java/ru.netology.web/OrderingCardDelivery.java
@@ -1,8 +1,6 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -12,10 +10,7 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OrderingCardDelivery {
-    @BeforeClass
-    public static void setup() {
-        Configuration.headless = true;
-    }
+
 
     private String generateDate(long addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
@@ -24,8 +19,7 @@ public class OrderingCardDelivery {
     @Test
     public void succsefulCardDelivery() {
         open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Мо");
-        $$(".menu-item__control").findBy(Condition.text("Москва")).click(); // выбор город из выпадающего списка
+        $("[data-test-id=city] input").setValue("Москва");
         String date = generateDate(5, "dd.MM.yyyy"); // генерация даты
         $("[data-test-id='date'] input").click();
         $("[data-test-id='date'] input").doubleClick(); // выделение содержимого
@@ -38,5 +32,26 @@ public class OrderingCardDelivery {
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
     }
+
+    @Test
+    public void succsefulCardDeliveryV2() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Мо");
+        $$(".menu-item__control").findBy(Condition.text("Москва")).click();
+        String date = generateDate(7, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").click();
+        if (!generateDate(4, "MM").equals(generateDate(7, "MM"))) {
+            $("[data-test-arrow='next']").click();
+        }
+        $$("[data-day]").findBy(Condition.text(generateDate(7, "d"))).click();
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+95000000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
+    }
+
 
 }
